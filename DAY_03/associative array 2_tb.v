9d3956fac52a961a8fedf7f85a5b1270c07bcd83
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-09-22 11:43:27 +0530

    Create associative array 2_tb.v

---
+++ DAY_03/associative array 2_tb.v
@@ -0,0 +1,11 @@
+// Code your testbench here
+// or browse Examples
+module associative_array_tb();
+  associative_array dut();
+  
+  initial begin
+    $dumpfile(associative_array.vcd);
+    $dumpvars(0);
+  end
+endmodule
+    
