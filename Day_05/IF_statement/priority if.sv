9eb7ff895dff191d1142564df49a137578977212
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-09-26 19:17:25 +0530

    Create priority if.sv

---
+++ Day_05/IF_statement/priority if.sv
@@ -0,0 +1,19 @@
+module priority_if;  
+  initial begin
+    int a, b;
+    a = 10;
+    b = 20;
+    priority if(a>b)
+      $display("a is greater than b");
+    else if(a<b)
+      $display("a is less than b");
+    else if(a<50)
+      $display("a is less than 50");
+    else 
+      $display("a is equal to b");
+  end
+endmodule
+
+
+output
+# a is less than b
