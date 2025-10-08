820b39b05dc41c4e1f02aedeb61d827ffa089a9b
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-10-08 12:16:39 +0530

    Create function calling statement.sv

---
+++ DAY_06/Function/function calling statement.sv
@@ -0,0 +1,19 @@
+module funtask();
+int data,address;
+function int funtc1(inout  int data,input int address);
+address=address+1;
+data=data+1;
+$display($time," address=%h data=%0d",address,data);
+endfunction
+initial begin
+address =0;
+data=9;
+#1;$display($stime,"call funtc1");
+void '(funtc1(data,address));
+end
+endmodule
+
+
+output
+# 1call funtc1
+# 1 address=00000001 data=10
