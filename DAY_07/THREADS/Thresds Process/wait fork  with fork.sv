6ac960663de30a53bfb6fe0a84ab32dca68d78c4
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-10-11 16:12:14 +0530

    Create wait fork  with fork.sv

---
+++ DAY_07/THREADS/Thresds Process/wait fork  with fork.sv
@@ -0,0 +1,31 @@
+module wait_fork_wait;
+
+  initial begin
+    $display("-----------------------------------------------------------------");
+    fork
+      //Process-1
+      begin
+        $display($time,"\tProcess-1 Started");
+        #5;
+        $display($time,"\tProcess-1 Finished");
+      end
+
+      //Process-2
+      begin
+        $display($time,"\tProcess-2 Started");
+        #20;
+        $display($time,"\tProcess-2 Finished");
+      end
+    join_any
+wait fork;
+    $display("-----------------------------------------------------------------");
+    $finish; //ends the simulation
+  end
+endmodule
+
+
+  output
+#0	Process-1 Started
+#0	Process-2 Started
+#5	Process-1 Finished
+#20	Process-2 Finished
