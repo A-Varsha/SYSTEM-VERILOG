3d82927f1197e90fabfb5f8ffd17d7c1555fdb04
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-10-13 14:12:46 +0530

    Create disable_fork.sv

---
+++ DAY_07/THREADS/Thresds Process/disable_fork.sv
@@ -0,0 +1,48 @@
+module disable_fork();
+initial begin
+//$display("-------------------------------------------");
+//fork1
+fork
+//process1
+begin
+$display($time,"\tprocess1 of fork1 started");
+#5;
+$display($time,"\tprocess1 of fork1 finished");
+end
+//process2
+begin
+$display($time,"\tprocess2 of fork1 started");
+#20;
+$display($time,"\tprocess2 of fork1 finished");
+end
+join_any
+//fork2
+fork
+//process1
+begin
+$display($time,"\tprocess1 of fork2 started");
+#5;
+$display($time,"\tprocess1 of fork2 finished");
+end
+//process2
+begin
+$display($time,"\tprocess2 of fork2 started");
+#20;
+$display($time,"\tprocess2 of fork2 finished");
+end
+join_none
+disable fork;
+//$display("------------------------------");
+$display($time,"\t after disable fork");
+//$display("------------------------------");
+end
+endmodule
+
+
+output
+ 0	process1 of fork1 started
+#0	process2 of fork1 started
+#5	process1 of fork1 finished
+#5	 after disable fork
+
+
