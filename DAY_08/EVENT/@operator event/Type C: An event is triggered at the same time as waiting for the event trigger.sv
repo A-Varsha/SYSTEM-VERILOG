8695c396d4e3b600a2f0b53344b91972bafc9e92
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-10-15 14:21:51 +0530

    Create Type C: An event is triggered at the same time as waiting for the event trigger.sv

---
+++ DAY_08/EVENT/@operator event/Type C: An event is triggered at the same time as waiting for the event trigger.sv
@@ -0,0 +1,24 @@
+module event_e();
+event e1;
+task process_a();
+$display("@%0t:before the event is trigger e1",$time);
+->e1;
+$display("@%0t:after the event is trigger e1",$time);
+endtask
+task process_b();
+$display("@%0t:waiting foe the event e1",$time);
+@e1;
+$display("@%0t:event is triggerd",$time);
+endtask
+initial begin
+fork
+process_a();
+process_b();
+join
+end
+endmodule
+
+output
+# @0:before the event is trigger e1
+# @0:after the event is trigger e1
+# @0:waiting foe the event e1
