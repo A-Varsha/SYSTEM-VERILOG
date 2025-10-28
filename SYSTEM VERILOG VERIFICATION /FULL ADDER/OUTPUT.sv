10ac05eb4e177ade34d31c6ef157832b4782fd97
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-10-28 16:40:59 +0530

    Create OUTPUT.sv

---
+++ SYSTEM VERILOG VERIFICATION /FULL ADDER/OUTPUT.sv
@@ -0,0 +1,49 @@
+#       Generator        
+# a=1 b=1 cin=0 sum=0 carry=0
+#        
+#        driver class signals         
+# a=1 b=1 cin=0 sum=0 carry=0
+#        
+#       monitor class signals        
+# a=1 b=1 cin=0 sum=0 carry=1
+#        
+# SCB: DUT=0/1 REF=0/1   PASS
+#       monitor class signals        
+# a=1 b=1 cin=0 sum=0 carry=1
+#        
+# SCB: DUT=0/1 REF=0/1   PASS
+#       monitor class signals        
+# a=1 b=1 cin=0 sum=0 carry=1
+#        
+# SCB: DUT=0/1 REF=0/1   PASS
+#       monitor class signals        
+# a=1 b=1 cin=0 sum=0 carry=1
+#        
+# SCB: DUT=0/1 REF=0/1   PASS
+#       monitor class signals        
+# a=1 b=1 cin=0 sum=0 carry=1
+#        
+# SCB: DUT=0/1 REF=0/1   PASS
+#       Generator        
+# a=1 b=1 cin=1 sum=0 carry=0
+#        
+#        driver class signals         
+# a=1 b=1 cin=1 sum=0 carry=0
+#        
+#       Generator        
+# a=0 b=1 cin=0 sum=0 carry=0
+#        
+#        driver class signals         
+# a=0 b=1 cin=0 sum=0 carry=0
+#        
+#       Generator        
+# a=1 b=0 cin=1 sum=0 carry=0
+#        
+#        driver class signals         
+# a=1 b=0 cin=1 sum=0 carry=0
+#        
+#       Generator        
+# a=1 b=1 cin=1 sum=0 carry=0
+#        
+#        driver class signals         
+# a=1 b=1 cin=1 sum=0 carry=0
