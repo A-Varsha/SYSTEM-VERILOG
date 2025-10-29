f285ffc2126aedb19ac8f72d6e851dc202dc32e4
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-10-29 10:19:57 +0530

    Create CLASS.sv

---
+++ DAY_10/OOPS concept/CLASS.sv
@@ -0,0 +1,31 @@
+// Code your design here
+class sv_class;
+  int x;
+  
+  task set (int i);
+    x=i;
+  endtask
+  
+  function  int get();
+    return x;
+  endfunction
+endclass
+  
+  module sv_class_ex;
+    sv_class class_1;
+    initial begin
+      sv_class class_2=new();
+      class_1=new();
+      
+      class_1.set(10);
+      class_2.set(20);
+      
+      $display("value of x =%0d",class_1.get());
+      $display("value of x =%0d",class_2.get());    
+    end
+  endmodule
+//op
+# value of x =10
+# value of x =20
+
+      
