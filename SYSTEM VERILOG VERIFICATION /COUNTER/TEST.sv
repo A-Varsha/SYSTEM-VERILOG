7673aa4cb09fe805d39a5e09847f7fb903528b65
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-10-30 19:01:09 +0530

    Add event g1 to the test program

--- SYSTEM VERILOG VERIFICATION /COUNTER/TEST.sv
+++ SYSTEM VERILOG VERIFICATION /COUNTER/TEST.sv
@@ -1,9 +1,11 @@
 program test ( intf vif);
-  environment env;  
+  environment env; 
+  event g1;
   
   initial begin
     env = new(vif); 
     #2;
     env.run();
+    @env.g1;
   end
 endprogram
