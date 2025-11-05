ecf4b62c728af2afba9190e9b9dd6b18c3978911
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-11-05 19:08:32 +0530

    Create class without using typedef.sv

---
+++ DAY_10/OOPS concept/TYPEDEF/class without using typedef.sv
@@ -0,0 +1,17 @@
+class c1;
+  c2 c;
+endclass
+class c2;
+  c1 cw;
+endclass
+module pack;
+  initial begin
+    c1 class1;
+    c2 class2;
+    $display("inside the class");
+  end
+endmodule
+
+//op
+** Error: Invalid
+            
