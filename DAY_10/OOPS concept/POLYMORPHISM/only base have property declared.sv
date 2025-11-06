87025d0831a3ef335d5f0817fa99612801616a77
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-11-06 10:49:30 +0530

    Create only base have property declared.sv

---
+++ DAY_10/OOPS concept/POLYMORPHISM/only base have property declared.sv
@@ -0,0 +1,30 @@
+class parent ;
+  bit[31:0]data;
+  int id;
+  function void display();
+    $display("value of data=%0d value of id=%0d",data,id);
+  endfunction
+endclass
+class child_class extends parent;
+  function void display();
+    $display("value of data=%0d value od id=%0d",data,id);
+  endfunction
+endclass
+module practice;
+  initial begin
+  parent p;
+  child_class cc;
+  cc=new();
+  p=cc;
+  p.data=100;
+  p.id=15;
+  
+  cc.data=120;
+  cc.id=330;
+  
+  p.display();
+  end
+endmodule
+//op
+# value of data=120 value of id=330
+  
