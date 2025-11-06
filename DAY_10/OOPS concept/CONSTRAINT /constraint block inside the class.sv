d5b2816e2e5eae3e1f063c4285cb4344b3c0a58f
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-11-06 12:25:55 +0530

    Create constraint block inside the class.sv

---
+++ DAY_10/OOPS concept/CONSTRAINT /constraint block inside the class.sv
@@ -0,0 +1,25 @@
+class packet;
+   rand bit[3:0]data;
+  constraint data_range{data>3;}
+endclass
+module pact;
+  packet pkt;
+  initial begin
+    pkt =new();
+    repeat(10)begin
+    pkt.randomize();
+      $display("data=%0d",pkt.data);
+  end
+  end
+endmodule
+//op
+# data=8
+# data=6
+# data=14
+# data=10
+# data=9
+# data=13
+# data=4
+# data=15
+# data=4
+# data=4
